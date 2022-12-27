d28c8f6cae74eabad0df4c2d0d9b0c616bfbda29
gigamaster <gigamaster@users.noreply.github.com>
2022-12-27 16:50:29 +0000

    deploy: 3d0fdb84af8f1011a6dacd2b73988fa96dcd28bb

---
+++ _nuxt/ResizeTop-2469f47c.mjs
@@ -0,0 +1,1 @@
+import{k as r,o,f as s,m as t}from"./entry-746bcc5f.mjs";const n={};function a(e,c){return o(),s("div",t(e.$attrs,{class:"resize-top"}),null,16)}var f=r(n,[["render",a]]);export{f as default};
